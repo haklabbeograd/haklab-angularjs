77a2406b88d789003cefb59fbae38f5099c13213
Dusan Stankovic <dusanstankovic90@gmail.com>
2015-08-26 09:45:13 +0200

    removed backend service, gitignore added

--- service/RadioniceServis.js
+++ service/RadioniceServis.js
@@ -1,22 +1,23 @@
-angular.module('haklab').service('RadioniceServis', function($http){
+'use strict';
+angular.module('haklab').service('RadioniceServis', function($http) {
   var self = this;
-  this.dohvatiRadionice = function(){
-    return $http.get('http://localhost:5000/radionice');
-  }
+  this.dohvatiRadionice = function() {
+    return $http.get('https://haklab.herokuapp.com/radionice');
+  };
 
-  this.dohvatiRadionicu = function(id){
-    return $http.get('http://localhost:5000/radionice/'+id);
-  }
+  this.dohvatiRadionicu = function(id) {
+    return $http.get('https://haklab.herokuapp.com/radionice/' + id);
+  };
 
-  this.dodajRadionicu = function(){
-    return $http.get('http://localhost:5000/test?ime=logika');
-  }
+  this.dodajRadionicu = function() {
+    return $http.get('https://haklab.herokuapp.com/test?ime=logika');
+  };
 
   return {
-    radionice : self.dohvatiRadionice,
-    radionica : self.dohvatiRadionicu,
-    dodaj : self.dodajRadionicu
-  }
+    radionice: self.dohvatiRadionice,
+    radionica: self.dohvatiRadionicu,
+    dodaj: self.dodajRadionicu
+  };
 
 
 });
