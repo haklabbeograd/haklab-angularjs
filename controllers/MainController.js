d245b220c5e439a530dd3a374a9b735659781518
Dusan Stankovic <dulerock@Dusans-MBP.lan>
2015-09-07 19:40:28 +0200

    add strict

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -1,20 +1,18 @@
-angular.module('haklab').controller('MainController',function($scope, RadioniceServis, $routeParams){
+'use strict';
+
+angular.module('haklab').controller('MainController', function($scope, RadioniceServis, $routeParams) {
 
   var id = $routeParams.broj || null;
 
   $scope.radionice = null;
 
-  RadioniceServis.dodaj().then(function(){
 
-  });
-
-  if(id != null){
-    RadioniceServis.radionica(id).then(function(response){
+  if (id !== null) {
+    RadioniceServis.radionica(id).then(function(response) {
       $scope.ovaRadionica = response.data;
     });
-  }else
-  {
-    RadioniceServis.radionice().then(function(response){
+  } else {
+    RadioniceServis.radionice().then(function(response) {
       $scope.radionice = response.data;
     });
   }
